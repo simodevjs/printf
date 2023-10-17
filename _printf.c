03cde76ed2966483975da46204da18a551190039
simodevjs <root@MohamedMAHLAs-MacBook-Pro-4.local>
2023-10-17 19:26:55 +0100

    simo

---
+++ _printf.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _printf- is a task from ALX it is a function for printing
+ * @format: its an identifier to seek.
+ * Return: 0 if succed.
+ */
+
+int _printf(const char *format, ...)
+{
+	return (0);
+}
